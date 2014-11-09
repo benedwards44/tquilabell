88f96280f5519de9424714548ad1d152a0c5f9e8
Ben Edwards <bedwards@tquila.com>
2014-11-09 21:09:08 +0000

    Added topic assignment

--- topic_subscription.py
+++ topic_subscription.py
@@ -3,13 +3,13 @@ from client import Client
 from bayeux.bayeux_client import BayeuxClient
 import json
 
-def subscribe_to_topic(sandbox, api_version):
+def subscribe_to_topic(sandbox, api_version, topic):
 
 	# Login to Salesforce to retrieve token and other variables
 	connection = login(sandbox)
 
 	bc = BayeuxClient(str(connection.instance_url) + '/cometd/' + str(api_version) + '.0/', str(connection.access_token))
-	bc.register('/topic/ClosedWonOpportunities', topic_callback)
+	bc.register('/topic/' + topic, topic_callback)
 	bc.start()
 
 def topic_callback(data):
